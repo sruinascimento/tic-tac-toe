6807fa90708f95215c05caceb5007aa600320e0a
Rui Nascimento <sruy19@gmail.com>
2023-02-28 19:32:11 -0300

    desafio devops finalizado

--- jogo-da-velha.py
+++ jogo-da-velha.py
@@ -1,6 +1,6 @@
-tabuleiro = ['-', '-', '-',
-             '-', '-', '-',
-             '-', '-', '-']
+from random import randint
+
+tabuleiro = ['-' for i in range(9)]
 
 jogador_x = "X"
 jogador_o = "O"
@@ -18,11 +18,19 @@ def mostrar_tabuleiro(tabuleiro):
 
 def realizar_jogada(tabuleiro, jogador):
     print(f'Turno do jogador {jogador}')
-    indice = int(input('Realize sua jogada, escolha um índice de 0-8 '))
-    if tabuleiro[indice] == "-":
-        tabuleiro[indice] = jogador
-    else:
-        print('Jogada inválida!')
+    indice = garantir_indice_valido(tabuleiro)
+    tabuleiro[indice - 1] = jogador
+   
+def garantir_indice_valido(tabuleiro) :
+    while True:
+        try:
+            indice = int(input('Realize sua jogada, escolha um índice de 1-9 '))
+            if (tabuleiro[indice-1] == '-' and indice in range(1, 10)):
+                return indice
+            else:
+                print('Jogada inválida!')
+        except:
+            print('Joga não permitida, digite valores de 1 a 9 somente!')
 
 def checar_vitoria(tabuleiro, jogador):
     if tabuleiro[0] == jogador and tabuleiro[1] == jogador and tabuleiro[2] == jogador or \
@@ -30,7 +38,9 @@ def checar_vitoria(tabuleiro, jogador):
         tabuleiro[6] == jogador and tabuleiro[7] == jogador and tabuleiro[8] == jogador or \
         tabuleiro[0] == jogador and tabuleiro[3] == jogador and tabuleiro[6] == jogador or \
         tabuleiro[1] == jogador and tabuleiro[4] == jogador and tabuleiro[7] == jogador or \
-        tabuleiro[2] == jogador and tabuleiro[5] == jogador and tabuleiro[8] == jogador:
+        tabuleiro[2] == jogador and tabuleiro[5] == jogador and tabuleiro[8] == jogador or \
+        tabuleiro[0] == jogador and tabuleiro[4] == jogador and tabuleiro[8] == jogador or \
+        tabuleiro[2] == jogador and tabuleiro[4] == jogador and tabuleiro[6] == jogador:
         print(f'O jogador {jogador} ganhou o jogo! :)')
         return True
     else:
@@ -43,13 +53,22 @@ def checar_empate(tabuleiro):
         print('Jogo empatado! XD')
         return True
 
+def bot_realiza_jogada(tabuleiro, jogador):
+    print(f'Turno do jogador {jogador}')
+    while True:
+        indice = randint(0, 8)
+        if tabuleiro[indice] == '-':
+            tabuleiro[indice] = jogador
+            break
+        
 mostrar_tabuleiro(tabuleiro)
 while True:
     realizar_jogada(tabuleiro, jogador_x)
     mostrar_tabuleiro(tabuleiro)
     if checar_vitoria(tabuleiro, jogador_x) or checar_empate(tabuleiro):
         break
-    realizar_jogada(tabuleiro, jogador_o)
+    # realizar_jogada(tabuleiro, jogador_o)
+    bot_realiza_jogada(tabuleiro, jogador_o)
     mostrar_tabuleiro(tabuleiro)
     if checar_vitoria(tabuleiro, jogador_o) or checar_empate(tabuleiro):
         break
